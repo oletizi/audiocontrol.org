8cc5ddef2eb545be1a41e6e82a680544872e172f
Orion Letizi <oletizi@mac.com>
2026-01-24 16:26:42 -0800

    Nice new website.

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://audiocontrol.org',
+  output: 'static',
+  adapter: netlify(),
+});
